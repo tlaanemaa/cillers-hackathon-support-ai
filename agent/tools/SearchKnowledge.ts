2b46829363f02e0d8e25fb9158b8c7af95caf272
Taavi Laanemaa <taavi.laanemaa@gmail.com>
2025-02-06 14:18:08 +0100

    Improved the knowledge base tool

--- agent/tools/SearchKnowledge.ts
+++ agent/tools/SearchKnowledge.ts
@@ -1,18 +1,20 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { Tool } from "../core/Tool";
 
 export class SearchKnowledge extends Tool {
   public readonly name = "search-knowledge-base";
-  public readonly description = "Ask a question from the knowledge-base";
+  public readonly description = "A tool to search the knowledge base for additional information.";
   public readonly arguments = {
     question: {
       type: "string",
-      description: "The question to ask the knowledge-base",
+      description: "The question to ask the knowledge base. Use this anytime you need additional context or verification.",
     },
     k: {
       type: "number",
-      description: "The number of results to return",
+      description: "The number of results to return. Use a higher number if broader context is needed.",
     }
   };
+
 
   public async run({ question, k }: { question: string, k: number }) {
     // Fetch the knowledge-base API
@@ -22,7 +24,15 @@ export class SearchKnowledge extends Tool {
     })
 
     const data = await response.json();
-    console.log(data);
-    return data;
+
+    // Filter responses
+    const CONFIDENCE_LIMIT = 0.5;
+    const content = data
+      .filter((result: any) => result.score > CONFIDENCE_LIMIT)
+      .map((result: any) => ({
+        content: result.document.content,
+        metadata: result.document.metadata,
+      }));
+    return content;
   }
 }
